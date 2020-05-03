7bc347584ef39a7992ff18ddf5bf73f9f584f56a
Jaeger <46061258+JaegerTao@users.noreply.github.com>
2020-05-03 20:33:36 +0800

    add login
    添加登录界面

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,19 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {
+	Button,
+	Form,
+	FormItem,
+	Input,
+	Tabs,
+	TabPane,
+	Message
+} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Tabs)
+Vue.use(TabPane)
+//弹框组件挂载到Vue原型对象上，每个组件可以通过this访问到$message进行弹框提示
+Vue.prototype.$message = Message
---
+++ src/views/Login.vue
@@ -0,0 +1,138 @@
+<template>
+	<div class="login-container">
+		<div class="login-box">
+			<!-- logo头像 -->
+			<div class="avatar_box">
+				<img src="../assets/image/logoITElite.jpg" alt="">
+			</div>
+			<div class="title">
+				教学质量评估管理系统
+			</div>
+			<!-- 登录表单 -->
+			<el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+				<!-- 学号 -->
+				<el-form-item prop="username">
+					<el-input v-model="loginForm.username" placeholder="请输入学号/教师号/督导号" prefix-icon="el-icon-user-solid"></el-input>
+				</el-form-item>
+				<!-- 密码 -->
+				<el-form-item prop="password">
+					<el-input v-model="loginForm.password" placeholder="请输入密码" type="password" prefix-icon="el-icon-lock"></el-input>
+				</el-form-item>
+				<!-- 按钮 -->
+				<el-form-item class="btns">
+					<el-button type="primary" @click="login">登录</el-button>
+					<el-button type="info" @click="resetLoginForm">重置</el-button>
+				</el-form-item>
+			</el-form>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				//登录表单数据绑定
+				loginForm: {
+					username:'2017110414',
+					password:'123456'
+				},
+				//表单验证规则对象
+				loginFormRules:{
+					//验证用户名是否合法
+					username: [
+						{ required: true, message: '请输入用户账号', trigger: 'blur' },
+						{ min: 6, max: 12, message: '请输入正确的账号', trigger: 'blur' }
+					],
+					//验证密码是否合法
+					password: [
+						{ required: true, message: '请输入密码', trigger: 'blur' },
+						{ min: 6, message: '密码最少6位', trigger: 'blur' }
+					]
+				},
+				
+			}
+		},
+		methods: {
+			//重置表单
+			resetLoginForm(){
+				this.$refs.loginFormRef.resetFields();
+			},
+			
+			//执行登录
+			login(){
+				//表单预验证
+				this.$refs.loginFormRef.validate(valid => {
+					// console.log(valid)
+					if(!valid) return; //验证不通过
+					//发起登陆请求
+					
+					//暂默认登录成功
+					this.$message.success("登录成功");
+					//服务器传回token，保存到sessionStorage(会话期间存储),只在当前网站打开期间生效
+					window.sessionStorage.setItem("token", 2017110414);
+					this.$router.push("/stuhome");
+				})
+			}
+		}
+	};
+</script>
+
+<style lang="less" scoped>
+	.login-container {
+		background-color: #2b4b6b;
+		height: 100%;
+	}
+
+	.login-box {
+		width: 450px;
+		height: 350px;
+		background-color: #FFFFFF;
+		border: 3px;
+		border-radius: 25px;
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+
+		.avatar_box {
+			height: 130px;
+			width: 130px;
+			border: 1px solid #eee;
+			border-radius: 50%;
+			padding: 10px;
+			box-shadow: 0 0 10px #ddd;
+			position: absolute;
+			left: 50%;
+			background-color: #FFFFFF;
+			transform: translate(-50%, -50%);
+
+			img {
+				height: 100%;
+				width: 100%;
+				border-radius: 50%;
+				background-color: #eee;
+			}
+		}
+		
+		.title {
+			position: absolute;
+			left: 50%;
+			top: 100px;
+			transform: translate(-50%);
+		}
+	}
+
+	.login_form {
+		position: absolute;
+		bottom: 0;
+		width: 100%;
+		padding: 0 20px;
+		box-sizing: border-box;
+	}
+
+	.btns {
+		display: flex;
+		justify-content: flex-end;
+	}
+</style>
