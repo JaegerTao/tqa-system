ac900c8b097d8ed1d026b07c534556290e817ff4
Zhao Chenxi <Kyliney@163.com>
2020-05-04 10:21:24 +0800

    modify stuHome

--- src/views/StudentHome/StuHome.vue
+++ src/views/StudentHome/StuHome.vue
@@ -1,7 +1,9 @@
 <template>
 	<div>
 		<el-button type="info" @click="logout">退出</el-button>
+		
 	</div>
+	
 </template>
 
 <script>
@@ -15,6 +17,29 @@
 	};
 </script>
 
-<style lang="less" scoped>
+<el-container>
+		<el-header>Header</el-header>
+		<el-main>Main</el-main>
+		<el-footer>Footer</el-footer>
+	</el-container>
 
+<style lang="less" scoped>
+	.el-header, .el-footer {
+		background-color: #B3C0D1;
+		color: #333;
+		text-align: center;
+		line-height: 60px;
+	}
+	
+	.el-main {
+		background-color: #E9EEF3;
+		color: #333;
+		text-align: center;
+		line-height: 160px;
+	}
+	
+	body > .el-container {
+		margin-bottom: 40px;
+	}
+	  
 </style>
